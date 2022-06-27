de75198d757f3493544244204ca7cdfa55e0aa71
ppjk <ppjk@ppjks-MacBook-Pro.local>
2022-06-27 20:38:24 +0200

    mouse slinger game

--- nextjs/pages/mouse_slinger.tsx
+++ nextjs/pages/mouse_slinger.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useRef, useState } from "react";
 import { FlexBox } from "../styles/containers";
 import { getRandomNumber } from "../helpers";
 import { count } from "console";
+import { map } from "lodash";
+import { off } from "process";
+import { brotliCompressSync } from "zlib";
 
 const fieldStyle = {
   height: 100,
@@ -14,6 +17,9 @@ const Target = ({
 }: {
   id: string;
   props: {
+    draw?: boolean;
+    fire?: boolean;
+    taunt?: boolean;
     size: number;
     top: number;
     left: number;
@@ -29,19 +35,28 @@ const Target = ({
       id={id}
       onClick={() => kill(id)}
       style={{
+        textAlign: "left",
         position: "absolute",
         left: props.left,
         top: props.top,
         borderRadius: 50,
         height: 10,
-        width: 10,
+        width: "max-content",
         fontSize: props.size,
-        backgroundColor: "red",
+        backgroundColor: "transparent",
         transition: `${props.travelTime}s all ease-in`,
         cursor: "pointer",
       }}
     >
-      {props.alive ? "🤠" : "😵 "}
+      {props.alive
+        ? props.taunt
+          ? "🤠"
+          : props.fire
+          ? "💥🤠"
+          : props.draw
+          ? "🔫 🤠"
+          : "🤠"
+        : "😵 "}
     </FlexBox>
   );
 };
@@ -51,24 +66,26 @@ type Target = {
   top: number;
   left: number;
   size: number;
+  taunt?: boolean;
+  draw?: boolean;
+  fire?: boolean;
   alive?: boolean;
+  delay: number;
   travelTime?: number;
 };
 
 const MouseSlinger = () => {
   const ref = useRef<HTMLDivElement>(null);
+  const blood = useRef<HTMLDivElement>(null);
   const [game, manageGame] = useState<{
+    ticks: number;
     status: number;
     count: number;
+    health: number;
+    bodyCount: number;
     targets: Target[];
-  }>({ status: 0, count: 0, targets: [] });
-
-  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (ref.current) {
-      const x = e.clientX - ref.current.offsetLeft;
-      const y = e.clientY - ref.current.offsetTop;
-    }
-  };
+    hit?: boolean;
+  }>({ status: 0, count: 0, bodyCount: 0, targets: [], health: 100, ticks: 1 });
 
   const killTarget = (id: string) => {
     let top = 0;
@@ -81,67 +98,141 @@ const MouseSlinger = () => {
       const x = ((offsetHeight - (target.top + target.size)) * 2) / (9.8 * 200);
       travelTime = Math.sqrt(x);
       const elem = document.getElementById(`${id}`);
-      console.log(elem);
       if (elem) {
         elem.style.animation = "0.5s jumpUp ease-out forwards";
       }
     }
+    const i = game.targets.findIndex((t) => t.id === id);
     manageGame({
       ...game,
-      targets: [
-        ...game.targets.slice(0, +id),
-        ...[{ ...game.targets[+id], alive: false, top, travelTime }],
-        ...game.targets.slice(+id + 1),
-      ],
+      bodyCount: game.bodyCount + 1,
+      targets: game.targets.map((t) =>
+        t.id === id ? { ...t, alive: false, top, travelTime } : t
+      ),
     });
   };
 
   useEffect(() => {
     if (game.targets.filter((t) => t.alive).length > 10) {
       // 2 = game over
-      manageGame({ ...game, status: 2 });
-      return;
+      //   manageGame({ ...game, status: 1 });
+      //   return;
     }
 
     const i = setInterval(() => {
       if (ref.current) {
+        let { ticks, health } = game;
+        if (health <= 0) {
+          return manageGame({ ...game, status: 2 });
+        }
         let count = game.count;
-        let newTargets: Target[] = [];
+        let hit = false;
+        let targets = [...game.targets];
         const makeTarget = (elem: HTMLDivElement) => {
-          console.log("make");
           const size = getRandomNumber(20, 40);
           const top = getRandomNumber(0, elem.offsetHeight - size);
           const left = getRandomNumber(0, elem.offsetWidth - size);
           const id = String(count);
-          newTargets.push({ id, top, left, size, alive: true });
+          targets.push({
+            id,
+            top,
+            left,
+            size,
+            alive: true,
+            delay: getRandomNumber(0, 5),
+          });
           count++;
         };
         // console.log(ref.current.offsetHeight);
-        for (let i = 0; i < getRandomNumber(1, 5); i++) {
-          makeTarget(ref.current);
+        if (game.ticks % 20 === 0) {
+          // spawn new enemies every second
+          for (let i = 0; i < getRandomNumber(1, 3); i++) {
+            makeTarget(ref.current);
+          }
         }
-        let targets = [...game.targets, ...newTargets];
-        if (game.targets.length > 20) {
-          // clean up corpses
-          targets = targets.filter((t) => t.alive);
+
+        if (game.targets.filter((t) => !t.alive).length > 25) {
+          // remove 5 corpses
+          const dead = targets
+            .filter((t) => !t.alive)
+            .slice(0, 5)
+            .map((d) => d.id);
+          targets = targets.filter((t) => dead.indexOf(t.id) === -1);
         }
-        manageGame({ ...game, count, targets });
+        if (blood.current) {
+          blood.current.style.opacity = "0";
+        }
+
+        for (const target of targets) {
+          if (!target.alive) continue;
+          if (target.delay > 0) {
+            //  target has just spawned and is passvie
+            target.delay--;
+            continue;
+          }
+          target.taunt = false;
+          const fastDraw = Math.random() <= 0.5;
+          if (!target.draw) {
+            if (game.ticks % (fastDraw ? 5 : 10) === 0) {
+              const draw = Math.random();
+              if (draw >= 0.75) {
+                target.draw = true;
+              }
+            }
+
+            continue;
+          } else {
+            if (target.fire) {
+              const shot = Math.random();
+              if (shot > 0.5) {
+                target.taunt = true;
+                if (blood.current) {
+                  blood.current.style.opacity = "0.5";
+                  health = health - getRandomNumber(1, 5);
+                }
+              }
+              // shoot here
+              target.fire = false;
+              target.draw = false;
+              continue;
+            }
+            if (game.ticks % (fastDraw ? 5 : 10) === 0) {
+              target.fire = true;
+            }
+          }
+          // draw
+
+          target.fire = true;
+        }
+
+        manageGame({ ...game, count, hit, targets, ticks: ticks + 1, health });
       }
-    }, 5000);
+    }, 100);
     return () => {
       clearInterval(i);
     };
   });
-
-  console.log(game.targets);
-
   return (
-    <FlexBox
-      style={{ height: "100%", overflow: "hidden" }}
-      ref={ref}
-      onMouseDown={handleClick}
-    >
-      {game.status === 2 && <div>gameover</div>}
+    <FlexBox style={{ height: "100%", overflow: "hidden" }} ref={ref}>
+      <FlexBox column gapSize={5} style={{ padding: 5 }}>
+        {game.status === 2 && <div>gameover</div>}
+        <div>hp: {game.health}</div>
+        <div>body count: {game.bodyCount}</div>
+      </FlexBox>
+      <div
+        ref={blood}
+        style={{
+          transition: `${game.status == 2 ? 5 : 0.5}s all`,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundColor: "red",
+          zIndex: game.status === 2 ? 1 : 0,
+          opacity: game.status === 2 ? 0.5 : 0,
+        }}
+      ></div>
       {game.targets.map((t) => {
         return (
           <Target
