4c83f1317e80c1a40e23003a098e0187d68d2082
ppjk <ppjk@ppjks-MacBook-Pro.local>
2023-11-21 23:48:40 +0100

    note

--- nextjs/styles/notifications.tsx
+++ nextjs/styles/notifications.tsx
@@ -13,3 +13,12 @@ export const Notification = styled.span<{ type?: string }>`
   border-radius: 5px;
   text-align: center;
 `;
+
+export const Warning = styled.span`
+  color: ${(props) => props.theme.darkGray};
+  background-color: null;
+  font-size: 15px;
+  height: max-content;
+  border-radius: 5px;
+  text-align: center;
+`;
